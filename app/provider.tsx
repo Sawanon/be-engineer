1b0ebb8d4adde273d9a05a798063dee846bc030d
gieyazka <pokket.1@gmail.com>
2024-09-19 21:45:07 +0700

    set up project

---
+++ app/provider.tsx
@@ -0,0 +1,51 @@
+// In Next.js, this file would be called: app/providers.jsx
+"use client";
+import {
+   isServer,
+   QueryClient,
+   QueryClientProvider,
+} from "@tanstack/react-query";
+import dayjs from "dayjs";
+import isSameOrAfter from "dayjs/plugin/isSameOrAfter";
+import isSameOrBefore from "dayjs/plugin/isSameOrBefore";
+import timezone from "dayjs/plugin/timezone";
+import { NextUIProvider } from "@nextui-org/react";
+
+import("dayjs/locale/th");
+dayjs.extend(timezone);
+dayjs.extend(isSameOrAfter);
+dayjs.extend(isSameOrBefore);
+
+function makeQueryClient() {
+   return new QueryClient({
+      defaultOptions: {
+         queries: {
+            // With SSR, we usually want to set some default staleTime
+            // above 0 to avoid refetching immediately on the client
+            staleTime: 60 * 1000,
+         },
+      },
+   });
+}
+
+let browserQueryClient: QueryClient | undefined = undefined;
+
+function getQueryClient() {
+   if (isServer) {
+      return makeQueryClient();
+   } else {
+      if (!browserQueryClient) browserQueryClient = makeQueryClient();
+      return browserQueryClient;
+   }
+}
+
+export default function Providers({ children }: { children: React.ReactNode }) {
+   const queryClient = getQueryClient();
+   return (
+      <NextUIProvider>
+         <QueryClientProvider client={queryClient}>
+            {children}
+         </QueryClientProvider>
+      </NextUIProvider>
+   );
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,22 @@
+const {nextui} = require("@nextui-org/react");
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+   content: [
+      "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+      "./components/**/*.{js,ts,jsx,tsx,mdx}",
+      "./app/**/*.{js,ts,jsx,tsx,mdx}",
+      "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+   ],
+   theme: {
+      extend: {
+         colors: {
+            background: "var(--background)",
+            foreground: "var(--foreground)",
+         },
       },
-    },
-  },
-  plugins: [],
+   },
+   darkMode: "class",
+   plugins: [nextui()],
 };
 export default config;
