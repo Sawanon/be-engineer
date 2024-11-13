dd8b3fe2782f5b2e532cd46deaeaf93333baff92
Sawanon <sawanon01@hotmail.com>
2024-11-13 19:38:52 +0700

    fixed bug

--- app/document/page.tsx
+++ app/document/page.tsx
@@ -1,10 +1,20 @@
 import DocumentComp from "@/components/Document";
+import { Spinner } from "@nextui-org/react";
+import { Suspense } from "react";
 
 const DocumentPage = () => {
    return (
       // <section className="absolute inset-0  flex flex-col bg-[#FAFAFA] overflow-x-hidden  ">
       <section>
-         <DocumentComp />
+         <Suspense
+            fallback={(
+               <div className={`absolute inset-0 bg-backdrop flex items-center justify-center`}>
+                  <Spinner />
+               </div>
+            )}
+            >
+               <DocumentComp />
+            </Suspense>
       </section>
    );
 };
